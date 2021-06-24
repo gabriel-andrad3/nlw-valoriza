5f850148ff212fe07bfe986f56d14d9d14390abb
Gabriel Andrade <gandradevini@gmail.com>
2021-06-24 15:06:58 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/test", (request, response) => {
+  return response.send("Olá NLW");
+});
+
+app.post("/test-post", (request, response) => {
+  return response.send("Olá NLW Post");
+});
+
+// http://localhost:3000
+app.listen(3000, () => console.log('Server is running nlw'));
